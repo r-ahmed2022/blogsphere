c28667329e5375fc500a94a9f4003ac1d76c0c83
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-07-11 21:28:08 +0530

    schema and corresponding foreign references

---
+++ db/migrate/20230711155557_add_foreign_references.rb
@@ -0,0 +1,8 @@
+class AddForeignReferences < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :comments, :author, foreign_key: { to_table: :users }
+    add_reference :comments, :post, foreign_key: true
+    add_reference :likes, :author, foreign_key: { to_table: :users }
+    add_reference :likes, :post, foreign_key: true
+  end
+end
