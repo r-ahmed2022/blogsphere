078c31280871aa522b9554e7c950ad19c52a579d
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-07-17 16:26:29 +0530

    created users and posts contollers

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
+
+  resources :users do 
+     resources :posts, only: [:index, :show]
+      post '/posts', to: 'posts#index'
+    # get '/posts/:id', to: 'posts#show'
+  end
+  resources :posts, only: [:show]
 end
