dc6187aee83cb2e8083ef55461e4e895bcf7a1cf
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-07-19 16:20:30 +0530

    initialised seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.new(
+    name: 'Riyaz',
+    email: 'riysaim@outlook.com',
+    password: 'microverse23',
+    password_confirmation: 'microverse23',
+    role: 'admin'
+  )
+  user.skip_confirmation! # If you have email confirmation enabled in Devise
+  user.save!
+ post = Post.create(
+    title: 'Demo Post',
+    text: 'This is demo for render site',
+    author: user
+)
+
+comment = Comment.create(
+    text: 'This is a comment for the default post.',
+    post: post,
+    author: user
+  )
+  
+
+user.posts << post
+user.comments << comment
