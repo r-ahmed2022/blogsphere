c0c792d88aa2f4e1716333406fef05b36a654cab
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-07-22 19:08:28 +0530

    updated show post spec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
+    @likes = @post.includes(:likes)
+    @comments= @posts.includes(:comments)
   end
 
   def show
