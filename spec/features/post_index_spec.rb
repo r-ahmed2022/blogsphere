b927f2d5e29dd7a406a2564e7a30ae4dd8620152
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-07-25 08:06:30 +0530

    updated post specs

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,61 +1,65 @@
 require 'rails_helper'
-RSpec.describe 'User index page', type: :feature do
-  context 'when there are users' do
+describe 'Post index page', type: :feature do
+  before :each do
+    @user = User.create(name: 'Ajaz', photo: 'ajaz.jpg', bio: 'Manager')
+    @post1 = Post.create(author: @user, title: 'Devise', text: 'for authentication')
+    @post2 = Post.create(author: @user, title: 'integration tests', text: 'Testing views of the rails application')
+    @post3 = Post.create(author: @user, title: 'unit test', text: 'for individual functions')
+    @post4 = Post.create(author: @user, title: 'Devise1', text: 'for authentication -1')
+    @post5 = Post.create(author: @user, title: 'Hello', text: 'for authentication -2')
+    @comment1 = Comment.create(post: @post1, author: @user, text: 'great achievment')
+    @comment2 = Comment.create(post: @post2, author: @user, text: 'little complicated than unit testing')
+    visit user_posts_path(@user)
+  end
+
+  it 'shows the user profile picture' do
+    expect(page).to have_css("img[src='#{@user.photo}']")
+  end
+
+  it 'shows the username of the user' do
+    expect(page).to have_content(@user.name)
+  end
+
+  it 'shows the number of posts for the user' do
+    expect(page).to have_content(@user.postscounter)
+  end
+
+  it 'shows the title of posts' do
+    expect(page).to have_content(@post1.title)
+    expect(page).to have_content(@post4.title)
+  end
+
+  it 'shows some of the posts text' do
+    expect(page).to have_content(@post2.text)
+    expect(page).to have_content(@post3.text)
+  end
+
+  it 'shows the first comments of a post' do
+    expect(page).to have_content(@comment1.text)
+    expect(page).to have_content(@comment2.text)
+  end
+
+  it 'shows the number of comments and likes for each post' do
+    expect(page).to have_content(@post1.commentscounter)
+    expect(page).to have_content(@post4.commentscounter)
+    expect(page).to have_content(@post2.likescounter)
+    expect(page).to have_content(@post3.likescounter)
+  end
+
+  it 'redirects to post show page of a clicked post' do
+    click_link(@post1.title)
+    expect(page).to have_current_path(user_post_path(@user, @post1))
+  end
+
+  it 'redirects to post show page of a clicked post' do
+    click_link(@post2.title)
+    expect(page).to have_current_path(user_post_path(@user, @post2))
+  end
+
+  context 'when there are more than 5 posts' do
     before :each do
-      @user1 = User.create(name: 'Riyaz Ali', photo: 'riyaz.jpg', bio: 'The developer',
-                           postscounter: 0)
-      @user2 = User.create(name: 'Basim', photo: 'basim.jpg', bio: 'SE',
-                           postscounter: 0)
-      @post1 = Post.create(author_id: @user1, title: 'Hello', text: 'This is a test post')
-      @comment1 = Comment.create(author_id: @user2, post_id: @post1, text: 'this is comment1')
-      @comment2 = Comment.create(author_id: @user2, post_id: @post1, text: 'this is comment2')
-      @comment3 = Comment.create(author_id: @user2, post_id: @post1, text: 'this is comment3')
-      @comment4 = Comment.create(author_id: @user2, post_id: @post1, text: 'this is comment4')
-      @comment5 = Comment.create(author_id: @user2, post_id: @post1, text: 'this is comment5')
-      visit user_posts_path(@user1, @post1)
-    end
-
-    it 'shows usernames of all other users' do
-      expect(page).to have_content(@user1.name)
-    end
-
-    it 'shows the profile picture for each user' do
-      expect(page).to have_css("img[src='#{@user1.photo}']")
-    end
-
-    it 'shows the number of posts for each user' do
-      expect(page).to have_content(@user1.postscounter)
-    end
-
-    it 'shows the post title' do
-      expect(page).to have_content(@post1.title)
-    end
-
-    it 'shows some of the post body' do
-      expect(page).to have_content(@post1.text)
-    end
-
-    it 'shows the first comments on a post' do
-      expect(page).to have_content(@comment1.text)
-      expect(page).to have_content(@comment2.text)
-      expect(page).to have_content(@comment3.text)
-      expect(page).to have_content(@comment4.text)
-      expect(page).to have_content(@comment5.text)
-    end
-
-    it 'shows comments count' do
-      expect(page).to have_content(@post1.commentscounter)
-    end
-    it 'shows likes count' do
-      expect(page).to have_content(@post1.likescounter)
-    end
-
-    #   it "shows  section for pagination if there are more posts than fit on the view" do
-    #     --------
-    #   end
-    it "redirects me to that post's show page" do
-      click_link(@post1.title.to_s)
-      expect(page).to have_current_path(user_post_path(@user1, post1))
+      @post6 = Post.create(author: @user, title: 'Ruby', text: 'little weird syntax')
+      visit user_posts_path(@user)
     end
   end
 end
--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -1,52 +1,30 @@
 require 'rails_helper'
-RSpec.describe 'User index page', type: :feature do
-  context 'when there are users' do
-    before :each do
-      @user1 = User.create(name: 'Riyaz Ali', photo: 'riyaz.jpg', bio: 'The developer',
-                           postscounter: 0)
-      @user2 = User.create(name: 'Basim', photo: 'basim.jpg', bio: 'SE',
-                           postscounter: 0)
-      @post1 = Post.create(author_id: @user1, title: 'Hello', text: 'This is a test post')
-      @comment1 = Comment.create(author_id: @user2, post_id: @post1, text: 'this is comment1')
-      @comment2 = Comment.create(author_id: @user2, post_id: @post1, text: 'this is comment2')
-      @comment3 = Comment.create(author_id: @user2, post_id: @post1, text: 'this is comment3')
-      @comment4 = Comment.create(author_id: @user2, post_id: @post1, text: 'this is comment4')
-      @comment5 = Comment.create(author_id: @user2, post_id: @post1, text: 'this is comment5')
-      visit user_post_path(@user1, @post1)
-    end
+describe 'Post show page', type: :feature do
+  before :each do
+    @user = User.create(name: 'Basim', photo: 'neo.jpg', bio: 'The One')
+    @post = Post.create(author: @user, title: 'Awakening', text: 'I choose the red pill')
+    @comment = Comment.create(author: @user, post: @post, text: 'Time to unplug')
+    visit user_post_path(@user, @post)
+  end
+  it 'shows the title of the post' do
+    expect(page).to have_content(@post.title)
+  end
 
-    it "shows post's title" do
-      expect(page).to have_content(@post1.title)
-    end
-    it "shows post's author" do
-      expect(page).to have_content(@post1.author.name)
-    end
+  it 'shows the author of the post' do
+    expect(page).to have_content(@post.author.name)
+  end
 
-    it 'shows comments count' do
-      expect(page).to have_content(@post1.commentscounter)
-    end
-    it 'shows likes count' do
-      expect(page).to have_content(@post1.likescounter)
-    end
+  it 'shows the number of comments and likes for the post' do
+    expect(page).to have_content(@post.commentscounter)
+    expect(page).to have_content(@post.likescounter)
+  end
 
-    it 'shows some of the post body' do
-      expect(page).to have_content(@post1.text)
-    end
+  it 'shows the body of the post' do
+    expect(page).to have_content(@post.text)
+  end
 
-    it 'shows  username of each commentor' do
-      expect(page).to have_content(@comment1.author.name)
-      expect(page).to have_content(@comment2.author.name)
-      expect(page).to have_content(@comment3.author.name)
-      expect(page).to have_content(@comment4.author.name)
-      expect(page).to have_content(@comment5.author.name)
-    end
-
-    it 'shows comment each commentor left' do
-      expect(page).to have_content(@comment1.text)
-      expect(page).to have_content(@comment2.text)
-      expect(page).to have_content(@comment3.text)
-      expect(page).to have_content(@comment4.text)
-      expect(page).to have_content(@comment5.text)
-    end
+  it 'shows the username of a commenter and their comment' do
+    expect(page).to have_content(@comment.author.name)
+    expect(page).to have_content(@comment.text)
   end
 end
