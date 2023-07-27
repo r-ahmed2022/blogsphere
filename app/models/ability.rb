597cc87608a217e1cb564e8c7e5159d76caee078
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-07-27 21:37:32 +0300

    Update ability.rb file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,9 +10,9 @@ class Ability
       can :manage, Post, author_id: user.id
     end
 
-    # can :show, Post do |post|
-    #  true
-    # end
+    can :show, Post do |post|
+     true
+    end
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
